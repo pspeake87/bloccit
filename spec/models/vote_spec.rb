a4a756701eb79de2790f2443b065b75a191fda73
Philip Speake <philip_speake@yahoo.com>
2015-09-17 11:32:04 -0700

    completed assignment-48

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,19 @@
+describe Vote do
+  describe "validations" do
+
+    before do
+      @post = Post.create(title: 'post title', body: 'post body')
+      @post.votes.create(value: 1)
+      @post.votes.create(value: -1)
+      @post.votes.create(value: 3)
+      @post.votes.create(value: -3)
+    end
+
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        expect( @post.up_votes ).to eq(1)
+        expect( @post.down_votes ).to eq(1)
+      end
+    end
+  end
+end
