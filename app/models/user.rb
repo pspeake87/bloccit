80264569ac42385dba35adb0524d8d494e7b83ac
Philip Speake <philip_speake@yahoo.com>
2015-09-18 13:27:29 -0700

    merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-  has_many :posts
-  has_many :comments
+  has_many :votes, dependent: :destroy       
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
 
   def admin?
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,8 +1,10 @@
+require 'rails_helper'
+
 describe Vote do
   describe "validations" do
 
     before do
-      @post = Post.create(title: 'post title', body: 'post body')
+      @post = Post.create(title: 'post title', body: 'post body Post bodies must be pretty long.')
       @post.votes.create(value: 1)
       @post.votes.create(value: -1)
       @post.votes.create(value: 3)
