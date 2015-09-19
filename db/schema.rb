627ff40753192e4946f37ac0efa8f94f890b9550
Philip Speake <philip_speake@yahoo.com>
2015-09-18 17:20:18 -0700

    completed checkpoint-49

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150918173212) do
+ActiveRecord::Schema.define(version: 20150918234408) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150918173212) do
     t.integer  "topic_id"
     t.integer  "summary_id"
     t.string   "image"
+    t.float    "rank"
   end
 
   add_index "posts", ["summary_id"], name: "index_posts_on_summary_id"
