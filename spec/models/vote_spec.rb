15f52e4d4b9492a65df9f130d2a8a04f849f2d72
Philip Speake <philip_speake@yahoo.com>
2015-09-21 11:13:16 -0700

    completed checkpoint-50

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,21 +1,30 @@
 require 'rails_helper'
 
 describe Vote do
-  describe "validations" do
 
-    before do
-      @post = Post.create(title: 'post title', body: 'post body Post bodies must be pretty long.')
-      @post.votes.create(value: 1)
-      @post.votes.create(value: -1)
-      @post.votes.create(value: 3)
-      @post.votes.create(value: -3)
-    end
+  include TestFactories
+  
+   describe "validations" do
+     describe "value validation" do
+       it "only allows -1 or 1 as values" do
+         up_vote = Vote.new(value: 1)
+         expect(up_vote.valid?).to eq(true)
 
-    describe "value validation" do
-      it "only allows -1 or 1 as values" do
-        expect( @post.up_votes ).to eq(1)
-        expect( @post.down_votes ).to eq(1)
-      end
-    end
-  end
+         down_vote = Vote.new(value: -1)
+         expect(down_vote.valid?).to eq(true)
+
+         invalid_vote = Vote.new(value: 2)
+         expect(invalid_vote.valid?).to eq(false)
+       end
+     end
+   end
+
+  describe 'after_save' do
+     it "calls `Post#update_rank` after save" do
+       post = associated_post
+       vote = Vote.new(value: 1, post: post)
+       expect(post).to receive(:update_rank)
+       vote.save
+     end
+   end
 end
