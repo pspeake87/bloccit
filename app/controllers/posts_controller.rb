eab6f41064ca275be50eeaa69807d3f1602a98d9
Philip Speake <philip_speake@yahoo.com>
2015-09-22 08:11:33 -0700

    fixed cp-46

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
     @comments = @post.comments
-    @comment = @post.comments.build
+    @comment = Comment.new #@post.comments.build
     authorize @post
   end
 
